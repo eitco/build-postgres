2088376bb0c1b425b03097759eb576cc4e3734c1
jschwarz <jschwarz@eitco.de>
2024-03-26 22:42:30 +0100

    parse version name from tag

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+tagName=$1
+echo "tagName = $tagName"
+
+vString=${tagName#REL}
+vString=${vString#_}
+majorVersion=${vString%%_*}
+vString=${vString#${majorVersion}_}
+minorVersion=${vString%%_*}
+vString=${vString#${minorVersion}}
+vString=${vString#_}
+patch=${vString%%-*}
+
+if [ -z "$patch" ]; then
+    version=$majorVersion.${minorVersion%%-*}
+else
+    version=$majorVersion.$minorVersion.$patch
+fi
+
+
+
 ./configure --prefix="$(pwd)"/pgsql --without-icu --without-zlib --without-readline --with-system-tzdata=/usr/share/zoneinfo/
 
 make
@@ -13,5 +33,5 @@ cd ..
 # distribute license
 cp COPYRIGHT pgsql
 
-tar -czvf "postgres-$1-$2.tar.gz" pgsql
+tar -czvf "postgres-$version-$2.tar.gz" pgsql
 
