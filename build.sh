be51fd3ac9c918926954f605c5daca2ac2e7b591
jschwarz <jschwarz@eitco.de>
2024-03-26 10:21:34 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+./configure --prefix="$(pwd)"/pgsql --without-zlib --without-readline --with-system-tzdata=/usr/share/zoneinfo/
+
+make
+make install
+
+cd contrib || exit
+make all
+make install
+cd ..
+
+chrpath --replace \\\$ORIGIN/../lib pgsql/bin/*
+
+tar -czvf postgres.tar.gz pgsql
+
