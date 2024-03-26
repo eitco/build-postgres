d38332600be4a1f2cf53e468e2d1f9575e8ad979
jschwarz <jschwarz@eitco.de>
2024-03-26 10:35:33 +0100

    add build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-./configure --prefix="$(pwd)"/pgsql --without-zlib --without-readline --with-system-tzdata=/usr/share/zoneinfo/
+./configure --prefix="$(pwd)"/pgsql --without-icu --without-zlib --without-readline --with-system-tzdata=/usr/share/zoneinfo/
+#./configure --prefix="$(pwd)"/pgsql --without-zlib --without-readline --with-system-tzdata=/usr/share/zoneinfo/
 
 make
 make install
